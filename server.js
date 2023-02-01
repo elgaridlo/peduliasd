deebc3f88bf241c4a6852e1cf288ea201c64b809
Elga Ridlo <elgaridlo@Elgas-MacBook-Air.local>
2023-02-01 16:55:16 +0700

    penyesuaian app untuk fly.io

--- server.js
+++ server.js
@@ -52,5 +52,5 @@ app.all('*', (req, res, next) => {
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`))
+const PORT = process.env.PORT || 3000
+app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} on port ${PORT}`))
