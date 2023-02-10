150b1034ec418b556906363d784e42abc4965869
Elga Ridlo <elgaridlo@Elgas-MacBook-Air.local>
2023-02-10 20:20:05 +0700

    mysql and docker

---
+++ getSeed.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+const Articles = require('./backend/models/articleModel')
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then(() => console.log('DB Connection Successfull'));
+
+const importData = async () => {
+    try {
+        const dataArticle = await Articles.find();
+        const artics = dataArticle.map((item) => ({
+            publish: item.publish,
+            title: item.title,
+            poster: item.poster,
+            content: item.content,
+            urlTitle: item.urlTitle
+        }))
+
+        await fs.writeFileSync('articles.json', JSON.stringify(artics), 'utf8',
+            function (err) {
+                if (err) throw err;
+                console.log('complete');
+            });
+
+        console.log('data successfuly loaded!');
+    } catch (err) {
+        console.log('error read db = ', err);
+    }
+    // it use for aggresively to stop the app
+    process.exit();
+};
+
+if (process.argv[2] === '--import') {
+    importData();
+}
